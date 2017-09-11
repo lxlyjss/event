c7bbe25f48f593d9f9869933cd7d589006d67273
lxlyjss <972128846@qq.com>
2017-09-11 15:54:07 +0800

    加入分享,报名加入数量,全部页面从后天获取数据

--- js/main.js
+++ js/main.js
@@ -53,9 +53,7 @@ $(function (){
 		            if (data._status == true) {//判断该用户已登录
 		                window.userInfo.name = data._user.nickname;//用户名
 		                window.userInfo.userId = data._user.id;//用户id
-		                //alert(window.userInfo.userId)
 		                window.userInfo.userImg = data._user.userImg;//用户头像
-		                //setUserInfo(window.userInfo.name, window.userInfo.userImg, window.userInfo.userId);//设置用户显示
 		            } else {//未登录则跳转
 		                //alert("你好,请重新登录!");
 		                window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxf509754d3d6886a6&redirect_uri=http%3a%2f%2fsport.72bike.com%2factivity%2ffront%2fwxLogin&response_type=code&scope=snsapi_login&connect_redirect=1#wechat_redirect";//微信登录
@@ -71,13 +69,63 @@ $(function (){
             //alert("你好,请重新登录!");
             window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxf509754d3d6886a6&redirect_uri=http%3a%2f%2fsport.72bike.com%2factivity%2ffront%2fwxLogin&response_type=code&scope=snsapi_login&connect_redirect=1#wechat_redirect";//微信登录
         }
-//        function setUserInfo(name,img,id){
-//            $(".header-user .user-info p img").attr("src",img);
-//        }
         if($("#loginOut").length > 0){
         	$("#loginOut").click(function (){
         		window.location.href = window.userInfo.url + "activity/front/loginout";
         	});
         }
+    }
+    //微信分享
+    $.fn.wxShare = function (){
+        $.ajax({
+            url:window.userInfo.url+"",
+            data:{
+                url:""
+            },
+            dataType:"json",
+            success:function (result){
+                wx.config({
+                    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                    appId: '', // 必填，公众号的唯一标识
+                    timestamp: '', // 必填，生成签名的时间戳
+                    nonceStr: '', // 必填，生成签名的随机串
+                    signature: '',// 必填，签名，见附录1
+                    jsApiList: [
+                        "onMenuShareTimeline",
+                        "onMenuShareAppMessage"
+                    ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+                });
+                wx.ready(function (){
+                    wx.onMenuShareTimeline({
+                        title: '', // 分享标题
+                        link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                        imgUrl: '', // 分享图标
+                        success: function () {
+                            // 用户确认分享后执行的回调函数
+                        },
+                        cancel: function () {
+                            // 用户取消分享后执行的回调函数
+                        }
+                    });
+                    wx.onMenuShareAppMessage({
+                        title: '', // 分享标题
+                        desc: '', // 分享描述
+                        link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                        imgUrl: '', // 分享图标
+                        type: '', // 分享类型,music、video或link，不填默认为link
+                        dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                        success: function () {
+                            // 用户确认分享后执行的回调函数
+                        },
+                        cancel: function () {
+                            // 用户取消分享后执行的回调函数
+                        }
+                    });
+                });
+            },
+            error:function (){
+                alert("获取微信分享接口失败!")
+            }
+        })
     }
 });
